e2f011fc51076725ba1e8e91ce957f6d088a4713
Julio Arellano <secondary.jcav@gmail.com>
2022-02-13 10:56:16 +0100

    cleaned environment.py and login.py

--- login_page/environment.py
+++ login_page/environment.py
@@ -1,20 +1,32 @@
 import json
-
+import time
+from behave import fixture, use_fixture
+from selenium import webdriver
 """
 Environment controls dealing with before all, before scenario, feature, steps.
 Useful for loading variables such as browser path
 """
 
 CONFIG_PATH = 'config.json'
+BROWSER_SLEEP = 10
+
+
+@fixture
+def selenium_browser_chrome(context):
+    with open(CONFIG_PATH) as config_file:
+        data = json.load(config_file)
+    # Assign chrome path
+    chrome_path = data["browser_path"]["chrome"]
+    context.browser = webdriver.Chrome(chrome_path)
+    yield context.browser
+    # cleanup:
+    time.sleep(BROWSER_SLEEP)
+    context.browser.quit()
 
 
 def before_all(context):
     """
-    Environment controls dealing with before all, before scenario, feature, steps
+    Launch browser fixture before any & all tests
     """
-
-    with open(CONFIG_PATH) as config_file:
-        data = json.load(config_file)
-    # Assign chrome path
-        context.chrome_path = data["browser_path"]["chrome"]
+    use_fixture(selenium_browser_chrome, context)
 
--- login_page/steps/login.py
+++ login_page/steps/login.py
@@ -1,27 +1,28 @@
 from behave import *
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver import Chrome
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from login_page.secrets import *
+
 
 # Steps defined in login.feature are implemented here
+START_URL = "URL goes here"
+START_URL_TITLE = "Page title"
 
 
 @given("login page can be reached")
 def open_login_page(context):
-    global driver
-    driver = Chrome(context.chrome_path)
-    driver.get("URL goes here")
+    driver = context.browser
+    driver.get(START_URL)
     try:
-        WebDriverWait(driver, 10).until(EC.title_is("Page Title"))
+        WebDriverWait(driver, 10).until(EC.title_is(START_URL_TITLE))
     except TimeoutException:
         raise AssertionError(f"Page title is {driver.title}")
 
 
 @given("user inputs name & password")
 def enter_credentials(context):
+    driver = context.browser
     try:
         username_input = driver.find_element_by_name("username")
         password_input = driver.find_element_by_name("password")
@@ -33,6 +34,7 @@ def enter_credentials(context):
 
 @when("user clicks login")
 def click_login_button(context):
+    driver = context.browser
     try:
         # login_button = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div/div[2]/form/button')
         login_button = driver.find_element_by_xpath('//*[@data-id="submit-login"]')
@@ -44,11 +46,13 @@ def click_login_button(context):
 
 @then("user remains in the same page")
 def failed_login(context):
-    assert driver.current_url == "Page Title", f"User has navigated to {driver.current_url}"
+    driver = context.browser
+    assert driver.current_url == "Page Titl", f"User has navigated to {driver.current_url}"
 
 
 @then(u'page goes to reset password')
 def check_reset_page(context):
+    driver = context.browser
     try:
         WebDriverWait(driver, 10).until(EC.url_contains("forgotPassword"))
     except TimeoutException:
@@ -57,6 +61,7 @@ def check_reset_page(context):
 
 @when(u'user clicks on forgot password')
 def click_on_forgot_password(context):
+    driver = context.browser
     try:
         forgot_password_button = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div/div[2]/form/div[3]/a')
         forgot_password_button.click()
@@ -66,6 +71,7 @@ def click_on_forgot_password(context):
 
 @when(u'user inputs email')
 def user_email_input(context):
+    driver = context.browser
     try:
         user_input = driver.find_element_by_name("username")
         user_input.send_keys("example_email@gmail.com")
@@ -75,6 +81,7 @@ def user_email_input(context):
 
 @when(u'user inputs password')
 def user_password_input(context):
+    driver = context.browser
     try:
         password_input = driver.find_element_by_name("password")
         password_input.send_keys("eoittnif")
@@ -84,12 +91,14 @@ def user_password_input(context):
 
 @when(u'user clicks on reset request button')
 def click_on_reset(context):
+    driver = context.browser
     reset_button = driver.find_element_by_css_selector('.simple-btn--login')
     reset_button.click()
 
 
 @then(u'page displays email sent message')
 def check_feedback_page(context):
+    driver = context.browser
     feedback_template = "Please check your inbox and follow the instructions to reset your password"
     try:
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".login__feedback-message")))
@@ -101,6 +110,7 @@ def check_feedback_page(context):
 
 @when(u'user clicks on free trial link')
 def click_free_trial_link(context):
+    driver = context.browser
     try:
         free_trial_link = driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div/div[2]/div[3]/a')
         free_trial_link.click()
@@ -110,6 +120,7 @@ def click_free_trial_link(context):
 
 @then(u'page navigates to free trial section')
 def check_free_trial_section(context):
+    driver = context.browser
     try:
         WebDriverWait(driver, 10).until(EC.title_contains("Start Free"))
     except TimeoutException:
